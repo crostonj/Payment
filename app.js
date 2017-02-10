d7dad8bd9c9579460dadb55bd303733776a4c967
Croston <21223@nelnet.net>
2017-02-10 11:17:34 -0700

    reverted cors code

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var app = express();
 var port = 4002;
 var cors = require('cors');
 
-app.use(cors);
+//app.use(cors);
 
-//app.use(function(req, res, next) {
-//  res.header("Access-Control-Allow-Origin", "*");
-//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//  next();
-//});
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.use(bodyParser.json());
